4a145553d98413336ac7b5b5d67846aa48c3afc5
Austin English <austienglish4@gmail.com>
2025-04-21 20:19:56 +0200

    add a placeholder employee details view component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { EmployeeDetailsComponent } from './components/employee-details/employee-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'employee/:employeeId',
+        component: EmployeeDetailsComponent,
+    },
+];
---
+++ src/app/components/employee-details/employee-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nr-employee-details',
+  imports: [],
+  templateUrl: './employee-details.component.html',
+  styleUrl: './employee-details.component.css'
+})
+export class EmployeeDetailsComponent {
+
+}
