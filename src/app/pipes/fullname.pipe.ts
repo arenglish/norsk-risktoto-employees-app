eadbdfe9ada384bed08c3005dfd134a0e15bb8cf
Austin English <austienglish4@gmail.com>
2025-04-22 11:07:16 +0200

    simplify the fullname pipe by using the employee util

--- src/app/pipes/fullname.pipe.ts
+++ src/app/pipes/fullname.pipe.ts
@@ -1,24 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { hasProp } from '../utilities/object.util';
+import { getEmployeeFullName } from '../utilities/employee.util';
 
 @Pipe({
     name: 'fullname',
 })
 export class FullnamePipe implements PipeTransform {
     transform(value: unknown, ...args: unknown[]): unknown {
-        if (typeof value !== 'object' || value === null) {
-            return '';
-        }
-
-        let fullname = '';
-        if (hasProp(value, 'first') && hasProp(value, 'last')) {
-            fullname = `${value.first} ${value.last}`;
-        }
-
-        if (hasProp(value, 'title')) {
-            fullname = `${value.title} ${fullname}`;
-        }
-
-        return fullname;
+        return getEmployeeFullName(value);
     }
 }
