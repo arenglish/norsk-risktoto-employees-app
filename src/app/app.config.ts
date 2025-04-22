723773156dfb577d036667d31282f7e7e11a3b14
Austin English <austienglish4@gmail.com>
2025-04-22 11:11:51 +0200

    clean up app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,26 +1,13 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import {
-    provideClientHydration,
-    withEventReplay,
-    withHttpTransferCacheOptions,
-    withIncrementalHydration,
-} from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideHttpClient(withFetch()),
         provideZoneChangeDetection({ eventCoalescing: true }),
-        provideRouter(routes, withComponentInputBinding()),
-        // provideClientHydration(
-        //     withEventReplay(),
-        //     withIncrementalHydration(),
-        //     withHttpTransferCacheOptions({
-        //         includePostRequests: true,
-        //     })
-        // ),
+        provideRouter(routes),
     ],
 };
