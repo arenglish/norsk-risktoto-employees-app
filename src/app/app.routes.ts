b7c46106dff7e134016c2dc385acc913173b757e
Austin English <austienglish4@gmail.com>
2025-04-22 02:22:43 +0200

    add routes for settings, about, and employee details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,23 @@
 import { Routes } from '@angular/router';
-import { EmployeeDetailsComponent } from './components/employee-details/employee-details.component';
+import { EmployeesComponent } from './pages/employees/employees.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+import { AboutComponent } from './pages/about/about.component';
 
 export const routes: Routes = [
     {
-        path: 'employee/:employeeId',
-        component: EmployeeDetailsComponent,
+        path: 'employees',
+        component: EmployeesComponent,
+    },
+    {
+        path: 'employees/:employeeId',
+        component: EmployeesComponent,
+    },
+    {
+        path: 'settings',
+        component: SettingsComponent,
+    },
+    {
+        path: 'about',
+        component: AboutComponent,
     },
 ];
