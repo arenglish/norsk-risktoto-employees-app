8360a8d3641de32db765377fd16af62d24110e0c
Austin English <austienglish4@gmail.com>
2025-04-22 07:49:17 +0200

    disable ssr for now (has some bugs with event listening that are happening even in demo applications)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,26 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import {
     provideClientHydration,
     withEventReplay,
+    withHttpTransferCacheOptions,
+    withIncrementalHydration,
 } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
     providers: [
+        provideHttpClient(withFetch()),
         provideZoneChangeDetection({ eventCoalescing: true }),
-        provideRouter(routes),
-        provideClientHydration(withEventReplay()),
+        provideRouter(routes, withComponentInputBinding()),
+        // provideClientHydration(
+        //     withEventReplay(),
+        //     withIncrementalHydration(),
+        //     withHttpTransferCacheOptions({
+        //         includePostRequests: true,
+        //     })
+        // ),
     ],
 };
